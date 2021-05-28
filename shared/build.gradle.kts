dfc268b3a691a038d21519c595c1d8a7ffb7b703
Elijah69 <elijahnightingale@gmail.com>
2021-05-28 16:40:08 +0300

    Revert "added kotlinx libs such as datatime, coroutines, serialization to common sourceset"
    This reverts commit 6122d0b7f1617008826de75f0135dd38d3ec7ce6.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,55 +1,42 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
-    kotlin("plugin.serialization") version("1.5.0")
+    kotlin("native.cocoapods")
 }
 
-object Versions {
-    val datatime = "0.2.1"
-    val junit = "4.13.2"
-    val coroutines = "1.5.0"
-    val serialization = "1.2.1"
-}
+version ="0.1"
 
 kotlin {
     //System.getenv().forEach { t, u -> println("$t $u") }
     android()
 
-    watchos() {
-        binaries {
-            framework {
-                baseName = "watch"
-            }
-        }
-    }
-    ios() {
-        binaries {
-            framework {
-                baseName = "ios"
-            }
-        }
-    }
-//    val iosTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
-//        if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
-//            ::iosArm64
-//        else
-//            ::iosX64
-//
-//    iosTarget("ios") {
+    watchosX64() { }
+//    ios() {
 //        binaries {
 //            framework {
-//                baseName = "shared"
+//                baseName = "ios"
 //            }
 //        }
 //    }
+    val iosTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
+        if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
+            ::iosArm64
+        else
+            ::iosX64
+
+    iosTarget("ios") { }
+    cocoapods {
+        watchos.deploymentTarget = "7.4"
+        ios.deploymentTarget = "14.1"
+        summary = "shared pod"
+        homepage = "no page"
+        frameworkName = "shared"
+        podfile = project.file("../../SepparatedApp(iOS)/Podfile")
+    }
     sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}")
-                implementation("org.jetbrains.kotlinx:kotlinx-datetime:${Versions.datatime}")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${Versions.serialization}")
-            }
-        }
+        val commonMain by getting
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
@@ -60,12 +47,12 @@ kotlin {
         val androidTest by getting {
             dependencies {
                 implementation(kotlin("test-junit"))
-                implementation("junit:junit:${Versions.junit}")
+                implementation("junit:junit:4.13.2")
             }
         }
         val iosMain by getting
         val iosTest by getting
-        val watchosMain by getting
+        val watchosX64Main by getting
     }
 }
 
